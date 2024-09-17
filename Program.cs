a6fe0ed317549c263fb6fb71d112065f481bbfc2
Qazowned05 <rivera16072903@gmail.com>
2024-09-16 23:29:03 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Problema02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Objeto objeto1 = new Objeto();
+
+            objeto1.name = "Diego";
+            objeto1.lasName = "Rivera";
+            objeto1.codigo = 3414;
+            objeto1.numero = 917287980;
+
+            string cadenajson = JsonConvert.SerializeObject(objeto1);
+            Console.WriteLine("el Json generado es : " + cadenajson);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejemplo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema02
+{
+    internal class Objeto
+    {
+        public int codigo {  get; set; }
+        public int numero { get; set; }
+        public string name { get; set; }
+        public string lasName { get; set; }
+
+    }
+}
